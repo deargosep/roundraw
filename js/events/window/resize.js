6f31127f7ec27ff12cfeab827ed280b84fecd0da
deargo <mchbtfl@gmail.com>
2023-01-31 18:48:45 +0500

    21125

--- js/events/window/resize.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('resize', function (event) {
-    tc.width = (innerWidth * devicePixelRatio);
-    tc.height = (innerHeight * devicePixelRatio);
-    c.width = (innerWidth * devicePixelRatio);
-    c.height = (innerHeight * devicePixelRatio);
-    draw(transform(view.wb))
-}, true);
